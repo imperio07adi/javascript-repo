421a586b5da0b7538e1c085fcf631546797155e0
Aditya Kokashe <adi310kokashe@gmail.com>
2025-05-03 11:10:30 +0000

    comparision

---
+++ 01_basics/04comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>= 1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+// console.log("2"< 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+// console.log(undefined < 0);
+
+/* so its not true that null is greater than 0 .... its javascript */
+
+console.log("2"===2);
+
+
+
