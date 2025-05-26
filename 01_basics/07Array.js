1ccf936e3d39c39d7e7945c49d4dbf0e3e606d72
Aditya Kokashe <adi310kokashe@gmail.com>
2025-05-26 16:29:51 +0000

    ezsnippet

---
+++ 01_basics/07Array.js
@@ -0,0 +1,50 @@
+//QUESTIONS---------------------------------
+// * Sum of all Numbers
+//     * Given: [1,2,3]
+//     * Output: 6
+// let arr = [1,2,3];
+// let a =0
+// for (let i=0;i<arr.length;i++){
+//     a += arr[i]
+// }
+// console.log(a);
+
+
+// * Find Max Number
+//     * Given: [4,9,2,7]
+//     * Output: 9
+let arr =[4,9,2,7]
+let max = 0
+for(let i=0;i<arr.length;i++){
+    if(max<arr[i]){
+        max = arr[i]
+    }
+}
+console.log(max);
+
+
+
+// * Find the Minimum Number
+//     * Given: [4,9,2,7]
+//     * Output: 2
+// * Count Even Numbers
+//     * Given: [1,2,3,4,5]
+//     * Output: 2 (2 and 4)
+// * Count Occurrences of a Number
+//     * Given: [1,2,2,3,2], target: 2
+//     * Output: 3
+// * Reverse an Array
+//     * Given: [1,2,3]
+//     * Output: [3,2,1]
+// * Remove Duplicates
+//     * Given: [1,2,2,3]
+//     * Output: [1,2,3]
+// * Check if  Array Contains a Value
+//     * Given: [5,7,8], Value :7
+//     * Output: true
+// * Double All Values
+//     * Given: [1,2,3]
+//     * Output: [2,4,6]
+// * Filter Out Odd Numbers
+//     * Given: [1,2,3,4]
+//     * Output: [2,4]
